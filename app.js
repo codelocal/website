51edc42f0d020d60f4db3a904b892a69af5ba96b
John Ellis <john@codelocal.co>
2017-07-19 17:34:20 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-app.js
+var port = 8080;
+var express = require('express');
+var app = express();
+
+//app configurations
+require('dotenv').config();
+app.set('view engine','pug');
+app.use('/static',express.static(__dirname+'/public'));
+
+//middleware
+app.use(bodyParser.urlencoded({extended:true}));
+
+//connec to db
+//var mongoose = require('mongoose');
+//mongoose.connect(process.env.DB_URI);
+
+app.listen(port, function(){
+  console.log('app started on '+port);
+})
